3e8000af1e49e8463e71697e8b604faec190480c
agubir87 <agustin.biritos@gmail.com>
2024-03-28 13:21:16 -0300

    Commit inicial

---
+++ src/main/java/utn/lab/backapptp1/BackAppTp1Application.java
@@ -0,0 +1,13 @@
+package utn.lab.backapptp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackAppTp1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackAppTp1Application.class, args);
+    }
+
+}
